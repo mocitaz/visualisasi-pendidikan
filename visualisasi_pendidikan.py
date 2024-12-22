ebdc01f1adb0597889f9a16288f6cc3c1eb8d412
Luthfi Fauzi <155809787+mocitaz@users.noreply.github.com>
2024-12-22 15:41:04 +0700

    Update visualisasi_pendidikan.py

--- visualisasi_pendidikan.py
+++ visualisasi_pendidikan.py
@@ -41,9 +41,13 @@ st.markdown(
 )
 
 # Membaca dataset
+st.subheader("📋 Dataset Pendidikan SD Indonesia 2023-2024")
+df = pd.read_csv("data_pendidikan_cleaned.csv")
+st.write(df)
+
 @st.cache_data
 def load_data():
-    return pd.read_csv("data_pendidikan_cleaned.csv")
+    return df
 
 df = load_data()
 
